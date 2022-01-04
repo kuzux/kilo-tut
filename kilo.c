4ed6a64f688b6242c88a2843b63496123c9bac21
Arda Cinar <kuzux92@gmail.com>
2022-01-04 21:36:26 +0300

    Somewhere around step 25

--- kilo.c
+++ kilo.c
@@ -5,11 +5,22 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
+// use die(NULL) for a successful exit
 void die(const char* msg) {
-	perror(msg);
-	exit(1);
+	// make sure we go to the top-left of the screen
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	if(msg) {
+		perror(msg);
+		exit(1);
+	} else {
+		exit(0);
+	}
 }
 
 void disableRawMode() {
@@ -34,19 +45,40 @@ void enableRawMode() {
 	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) < 0) die("tcsetattr");
 }
 
+char editorReadKey() {
+	char c = 0;
+	int rc = read(STDIN_FILENO, &c, 1);
+	if(rc < 0 && errno != EAGAIN) die("read");
+
+	return c;
+}
+
+void editorDrawRows() {
+	for (int i=0; i<24; ++i) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+	write(STDOUT_FILENO, "\x1b[H", 3); // reposition cursor
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorProcesssKeypress() {
+	char c = editorReadKey();
+	if(c==CTRL_KEY('q')) die(NULL);
+}
+
 int main() {
 	enableRawMode();
 
 	for(;;) {
-		char c = '\0';
-		int rc = read(STDIN_FILENO, &c, 1);
-		if(rc < 0 && errno != EAGAIN) die("read");
-		if(c=='q') break;
-
-		if(rc == 1) {
-			if(iscntrl(c)) printf("%d\r\n", c);
-			else printf("%d ('%c')\r\n", c, c);
-		}
+		editorRefreshScreen();
+		editorProcesssKeypress();
 	}
 
 	return 0;
